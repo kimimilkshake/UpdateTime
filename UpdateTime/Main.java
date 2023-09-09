29f371a64c016797142bb11a7f9b5a519654b64f
kimimilkshake <131957149+kimimilkshake@users.noreply.github.com>
2023-09-09 23:55:27 +0800

    Update Main.java

--- UpdateTime/Main.java
+++ UpdateTime/Main.java
@@ -1,13 +1,1 @@
-public static void main(String[] args) {
-        AdvancedTime time1 = new AdvancedTime(); // Midnight
-        time1.displayTime12();
-        time1.displayTime24();
 
-        AdvancedTime time2 = new AdvancedTime(3600); //  1 hour 
-        time2.displayTime12();
-        time2.displayTime24();
-
-        AdvancedTime time3 = new AdvancedTime(3, 30, "PM"); //  3:30 PM
-        time3.displayTime12();
-        time3.displayTime24();
-}
